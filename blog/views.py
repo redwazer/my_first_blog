78ac4cb12e221498caf826afd4744909f6e01206
redwazer <redwazer7@gmail.com>
2019-06-08 13:06:30 +0600

    Django first commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+	posts = Post.objects.all()
+	print(posts)
+
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Create your views here.
